1ee835cd94f68549b011e0c50b2c754cb191ab4c
nygupta <nygupta21@gmai.com>
2021-06-24 18:20:21 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <h1>
-      Box-Office!
-    </h1>
+    <Switch>
+      <Route exact path='/'>
+        This is home page!
+      </Route>
+      <Route exact path='/starred'>
+        This is starred!
+      </Route>
+      <Route>
+        <h1>Error 404!</h1>
+        <h2>Page not Found!</h2>
+      </Route>
+    </Switch>
   );
 }
 
