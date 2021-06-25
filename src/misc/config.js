481f27adbeca61fd28f560b36409821d25e546f8
nygupta <nygupta21@gmai.com>
2021-06-25 23:29:58 +0530

    added loading & error messages to show page

--- src/misc/config.js
+++ src/misc/config.js
@@ -3,6 +3,6 @@ const API_BASE_URL = 'https://api.tvmaze.com';
 export async function apiGet(queryString) {
     const responce = await fetch(`${API_BASE_URL}${queryString}`)
         .then(r => r.json());
-
+        
     return responce;
 }
