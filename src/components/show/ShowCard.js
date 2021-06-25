d8e81904c17a556211db2ecf56a42223487bfda8
nygupta <nygupta21@gmai.com>
2021-06-26 00:47:12 +0530

    styled components

--- src/components/show/ShowCard.js
+++ src/components/show/ShowCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { StyledShowCard } from './ShowCard.styled';
 
 const ShowCard = ({ id, image, name, summary }) => {
     const summaryAsText = summary
@@ -7,17 +8,17 @@ const ShowCard = ({ id, image, name, summary }) => {
       : 'No description';
   
     return (
-      <div>
-        <div>
+      <StyledShowCard>
+        <div className='img-wrapper' >
           <img src={image} alt="show" />
         </div>
         <h1>{name}</h1>
         <p>{summaryAsText}</p>
-        <div>
+        <div className='btns' >
           <Link to={`/show/${id}`}>Read more</Link>
           <button type="button">Star me</button>
         </div>
-      </div>
+      </StyledShowCard>
     );
 };
   
--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ShowCard from './ShowCard';
+import { FlexGrid } from '../styled';
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
 
 const ShowGrid = ({data}) => {
     return (
-        <div>{
+        <FlexGrid>{
             data.map(({show}) => (
                 <ShowCard 
                 key={show.id} 
@@ -13,7 +14,7 @@ const ShowGrid = ({data}) => {
                 image={show.image ? show.image.medium : IMAGE_NOT_FOUND} 
                 summary={show.summary}
             />))
-        }</div>
+        }</FlexGrid>
     );
 };
 
